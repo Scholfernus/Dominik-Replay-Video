298de50fee0bdb40e339d45246e244b9f0477448
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-03 21:28:14 +0200

    pozostałe pliki

---
+++ src/test/java/com/example/videofilm/VideoFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videofilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
